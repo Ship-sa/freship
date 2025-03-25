c1f27a0c92d8dac806da9788ebeaef01f0b77c55
Heni <Heni0717>
2025-03-25 14:19:26 +0900

    Feat: Store Entity, Repository 추가

---
+++ src/main/java/me/yeon/freship/store/domain/Store.java
@@ -0,0 +1,38 @@
+package me.yeon.freship.store.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.yeon.freship.common.domain.BaseEntity;
+import me.yeon.freship.member.domain.Member;
+
+@Entity
+@Table(name = "store")
+@Getter
+@NoArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String bizRegNum;
+
+    @Column(nullable = false)
+    private String address;
+
+    public Store(String name, String bizRegNum, String address) {
+        this.name = name;
+        this.bizRegNum = bizRegNum;
+        this.address = address;
+    }
+
+}
