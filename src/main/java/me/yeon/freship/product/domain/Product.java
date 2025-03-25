339880f985f896de7228ea5f8b0d613f6cdc345a
Heni <Heni0717>
2025-03-25 14:38:28 +0900

    Feat: Product Entity, Repository 추가

---
+++ src/main/java/me/yeon/freship/product/domain/Product.java
@@ -0,0 +1,54 @@
+package me.yeon.freship.product.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.yeon.freship.common.domain.BaseEntity;
+import me.yeon.freship.store.domain.Store;
+
+@Entity
+@Table(name = "product")
+@Getter
+@NoArgsConstructor
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    private String imgUrl;
+
+    private String description;
+
+    public Product(String name, Integer quantity, Status status, Category category, Integer price, String imgUrl, String description) {
+        this.name = name;
+        this.quantity = quantity;
+        this.status = status;
+        this.category = category;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/me/yeon/freship/product/domain/Status.java
@@ -0,0 +1,6 @@
+package me.yeon.freship.product.domain;
+
+public enum Status {
+    ON_SALE,
+    STOPPED
+}
