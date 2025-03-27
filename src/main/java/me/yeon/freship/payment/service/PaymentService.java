d4f441325a69dc19e113f75f6eacf3baf3589f82
김종연 <bellopenerr@gmail.com>
2025-03-27 12:00:17 +0900

    Refactor: 파일 정리 및 pg 요청 메서드 분리

--- src/main/java/me/yeon/freship/payment/service/PaymentService.java
+++ src/main/java/me/yeon/freship/payment/service/PaymentService.java
@@ -5,7 +5,7 @@ import me.yeon.freship.common.domain.constant.ErrorCode;
 import me.yeon.freship.common.exception.ClientException;
 import me.yeon.freship.payment.domain.CheckoutResponse;
 import me.yeon.freship.payment.domain.ConfirmResponse;
-import me.yeon.freship.payment.infrastructure.persistence.PaymentHistoryRepository;
+import me.yeon.freship.payment.infrastructure.PaymentHistoryRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
@@ -29,6 +29,21 @@ public class PaymentService {
         // TODO: 넘어온 amount랑 주문서 비교
         // 에러 상황에서의 처리
 
+        ConfirmResponse confirmResponse = sendConfirmRequest(checkoutResponse);
+
+
+        // 여기서부터 트랜잭션이 될 것임
+        // 결제 정보 저장
+        paymentHistoryRepository.save(confirmResponse.toSuccessHistory(1L));
+
+        // 주문 정보 저장
+
+        // 재고 차감
+
+        return confirmResponse;
+    }
+
+    private ConfirmResponse sendConfirmRequest(CheckoutResponse checkoutResponse) {
         ConfirmResponse confirmResponse = restClient.post()
                 .uri("https://api.tosspayments.com/v1/payments/confirm")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -41,16 +56,6 @@ public class PaymentService {
                     throw new ClientException(ErrorCode.PAY_CONFIRM_FAILED);
                 }))
                 .body(ConfirmResponse.class);
-
-
-        // 여기서부터 트랜잭션이 될 것임
-        // 결제 정보 저장
-        paymentHistoryRepository.save(confirmResponse.toSuccessHistory(1L));
-
-        // 주문 정보 저장
-
-        // 재고 차감
-
         return confirmResponse;
     }
 }
