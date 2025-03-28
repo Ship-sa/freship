8ffd429c4118ec25ccace9fb31eb5624f240e4b3
김종연 <bellopenerr@gmail.com>
2025-03-28 10:59:34 +0900

    Feat: Order-Product간 연관관계 끊음

--- src/main/java/me/yeon/freship/orders/infrastructure/OrderRepository.java
+++ src/main/java/me/yeon/freship/orders/infrastructure/OrderRepository.java
@@ -2,13 +2,7 @@ package me.yeon.freship.orders.infrastructure;
 
 import me.yeon.freship.orders.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
 
-    @Query("select o from Order o join fetch o.product where o.id=:id")
-    Optional<Order> findByIdWithProduct(@Param("id") Long id);
 }
