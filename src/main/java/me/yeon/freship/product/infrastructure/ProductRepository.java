258501a91ce2b01d8e861eb2163b23b676e2b920
김종연 <bellopenerr@gmail.com>
2025-03-28 15:59:55 +0900

    Feat: Query 담당 api 구현

--- src/main/java/me/yeon/freship/product/infrastructure/ProductRepository.java
+++ src/main/java/me/yeon/freship/product/infrastructure/ProductRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
@@ -16,6 +18,9 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
     Page<Product> findByCategory(Category category, Pageable pageable);
 
     boolean existsByStoreAndName(Store store, String name);
+
+    @Query("select p from Product p join fetch p.store s join s.member where p.id=:productId")
+    Optional<Product> findByIdWithStoreAndOwner(@Param("productId") Long id);
 
     @Query("SELECT p FROM Product p WHERE p.id in :productIds")
     List<Product> findProductsByRank(List<Long> productIds);
