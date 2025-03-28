6b75721d6f741e7f9d7492625a8bbbef1b1984e7
김종연 <bellopenerr@gmail.com>
2025-03-28 16:44:35 +0900

    Chore: rebase 충돌 해결

--- src/main/java/me/yeon/freship/payment/infrastructure/persistence/PaymentHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package me.yeon.freship.payment.infrastructure.persistence;
-
-import me.yeon.freship.payment.domain.PayHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentHistoryRepository extends JpaRepository<PayHistory, Long> {
-}
