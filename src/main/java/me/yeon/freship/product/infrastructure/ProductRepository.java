a7c0b4469520445e277e29bba0a353a9a28c1c3f
kyn1013 <yn112511@naver.com>
2025-03-29 15:51:06 +0900

    Feat : 상품 상세조회 및 조회수 기준 인기 상품 조회 기능 추가

--- src/main/java/me/yeon/freship/product/infrastructure/ProductRepository.java
+++ src/main/java/me/yeon/freship/product/infrastructure/ProductRepository.java
@@ -2,7 +2,12 @@ package me.yeon.freship.product.infrastructure;
 
 import me.yeon.freship.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+    @Query("SELECT p FROM Product p WHERE p.id in :productIds")
+    List<Product> findProductsByRank(List<Long> productIds);
 }
