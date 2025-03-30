6fb5504135237ced038ed425bae5b185e25d7e0a
kyn1013 <yn112511@naver.com>
2025-03-30 19:16:18 +0900

    Chore : Rebase 충돌 해결

--- src/main/java/me/yeon/freship/common/utils/RedisUtils.java
+++ src/main/java/me/yeon/freship/common/utils/RedisUtils.java
@@ -6,14 +6,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class RedisUtils {
 
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     // 사용자 검색 기록 저장 (동일한 검색어 중복 방지)
     public void saveSearchHistory(Long userId, String searchKeyword) {
@@ -31,8 +33,52 @@ public class RedisUtils {
 
     // 인기 검색어 조회 (상위 10개)
     public List<String> getTopSearchKeywords() {
-        Set<String> popularSearch = redisTemplate.opsForZSet().reverseRange("rank", 0, 9);
+        Set<String> popularSearch = redisTemplate.opsForZSet()
+                .reverseRange("rank", 0, 9)
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.toSet());
         return popularSearch != null ? new ArrayList<>(popularSearch) : new ArrayList<>();
+    }
+
+    public void setReadCount(Long id){
+        redisTemplate.opsForZSet().add("product:readCount", "product:" + id, 1);
+    }
+
+    public Long getReadCount(Long productId){
+        return redisTemplate.opsForZSet().score("product:readCount", "product:" + productId).longValue();
+    }
+
+    public Long addReadCount(Long productId){
+        return redisTemplate.opsForZSet().incrementScore("product:readCount", "product:" + productId, 1).longValue();
+    }
+
+    public boolean notExistsReadCount(Long productId){
+        // 값이 존재하지 않는다면 null 반환
+        Double currentReadCount = redisTemplate.opsForZSet().score("product:readCount", "product:" + productId);
+        return currentReadCount == null;
+    }
+
+    // 조회수 상위 10개의 상품 id 리스트 조회
+    public List<Long> findProductIds() {
+        Set<Object> objectSet = redisTemplate.opsForZSet().reverseRange("product:readCount",0, 9);
+        if (objectSet == null) {
+            return Collections.emptyList();
+        }
+
+        //"product:7" -> ["product", "7"] -> ["7"]로 변환
+        List<Long> productIdList = new ArrayList<>();
+        for (Object o : objectSet) {
+            String value = (String) o;
+            String[] parts = value.split(":");
+            productIdList.add(Long.parseLong(parts[1]));
+        }
+        return productIdList;
+    }
+
+    public Boolean isNotViewed (Long productId, Long userId) {
+        Boolean isNotViewed = redisTemplate.opsForValue().setIfAbsent("product:" + productId + ":user:" + userId, "viewed");
+        return isNotViewed;
     }
 
     // 자정에 캐시 리셋
@@ -54,5 +100,15 @@ public class RedisUtils {
         if (Boolean.TRUE.equals(redisTemplate.hasKey("rank"))) {
             redisTemplate.opsForZSet().removeRange("rank", 0, -1);
         }
+
+        Set<String> visitorKeys = redisTemplate.keys("product*");
+        if (visitorKeys != null && !visitorKeys.isEmpty()) {
+            redisTemplate.delete(visitorKeys);
+        }
+
+        if (Boolean.TRUE.equals(redisTemplate.hasKey("product:readCount"))) {
+            redisTemplate.opsForZSet().removeRange("product:readCount", 0, -1);
+        }
     }
+
 }
---
+++ src/main/java/me/yeon/freship/product/domain/ProductRankResponse.java
@@ -0,0 +1,57 @@
+package me.yeon.freship.product.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ProductRankResponse {
+
+    private final Long id;
+    private final Long rank;
+    private final String name;
+    private final Integer quantity;
+    private final Status status;
+    private final Category category;
+    private final Integer price;
+    private final String imgUrl;
+    private final String description;
+
+    @Builder
+    public ProductRankResponse(Long id, Long rank, String name, Integer quantity,
+                               Status status, Category category, Integer price, String imgUrl, String description) {
+        this.id = id;
+        this.rank = rank;
+        this.name = name;
+        this.quantity = quantity;
+        this.status = status;
+        this.category = category;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.description = description;
+    }
+
+    public static List<ProductRankResponse> toProductRankResponseList(List<Product> products){
+        List<ProductRankResponse> readCountResponses = new ArrayList<>();
+        Long rank = 1L;
+        for (Product product : products) {
+            ProductRankResponse productReadCountResponse = ProductRankResponse.builder()
+                    .id(product.getId())
+                    .rank(rank++)
+                    .name(product.getName())
+                    .quantity(product.getQuantity())
+                    .category(product.getCategory())
+                    .price(product.getPrice())
+                    .status(product.getStatus())
+                    .price(product.getPrice())
+                    .imgUrl(product.getImgUrl())
+                    .description(product.getDescription())
+                    .build();
+
+            readCountResponses.add(productReadCountResponse);
+        }
+        return readCountResponses;
+    }
+}
